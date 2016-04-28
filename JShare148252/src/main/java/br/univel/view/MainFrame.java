67fdb82dde96d195c06e73792521388aad21d5bc
Aureo Júnior <aureojr1@gmail.com>
2016-04-28 12:54:00 -0300

    Layout + exemplos do professor

--- JShare148252/src/main/java/br/univel/view/MainFrame.java
+++ JShare148252/src/main/java/br/univel/view/MainFrame.java
@@ -39,7 +39,7 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 606, 433);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,10 +52,18 @@ public class MainFrame extends JFrame {
 		contentPane.add(pnlLeft, BorderLayout.WEST);
 		GridBagLayout gbl_pnlLeft = new GridBagLayout();
 		gbl_pnlLeft.columnWidths = new int[]{117, 0};
-		gbl_pnlLeft.rowHeights = new int[]{25, 0, 0, 0, 0};
+		gbl_pnlLeft.rowHeights = new int[]{0, 25, 0, 0, 0, 0};
 		gbl_pnlLeft.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_pnlLeft.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_pnlLeft.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		pnlLeft.setLayout(gbl_pnlLeft);
+		
+		JButton btnHome = new JButton("");
+		GridBagConstraints gbc_btnHome = new GridBagConstraints();
+		btnHome.setIcon(new ImageIcon(MainFrame.class.getResource("/icons/64/home-icon.png")));
+		gbc_btnHome.insets = new Insets(0, 0, 5, 0);
+		gbc_btnHome.gridx = 0;
+		gbc_btnHome.gridy = 0;
+		pnlLeft.add(btnHome, gbc_btnHome);
 		
 		JButton btnSeach = new JButton("");
 		btnSeach.setIcon(new ImageIcon(MainFrame.class.getResource("/icons/64/search-icon.png")));
@@ -64,27 +72,30 @@ public class MainFrame extends JFrame {
 		gbc_btnSeach.insets = new Insets(0, 0, 5, 0);
 		gbc_btnSeach.anchor = GridBagConstraints.NORTH;
 		gbc_btnSeach.gridx = 0;
-		gbc_btnSeach.gridy = 0;
+		gbc_btnSeach.gridy = 1;
 		pnlLeft.add(btnSeach, gbc_btnSeach);
 		
-		JButton btnDownloads = new JButton("New button");
+		JButton btnDownloads = new JButton("");
 		GridBagConstraints gbc_btnDownloads = new GridBagConstraints();
+		btnDownloads.setIcon(new ImageIcon(MainFrame.class.getResource("/icons/64/download-2-icon.png")));
 		gbc_btnDownloads.insets = new Insets(0, 0, 5, 0);
 		gbc_btnDownloads.gridx = 0;
-		gbc_btnDownloads.gridy = 1;
+		gbc_btnDownloads.gridy = 2;
 		pnlLeft.add(btnDownloads, gbc_btnDownloads);
 		
-		JButton btnChat = new JButton("New button");
+		JButton btnChat = new JButton("");
 		GridBagConstraints gbc_btnChat = new GridBagConstraints();
+		btnChat.setIcon(new ImageIcon(MainFrame.class.getResource("/icons/64/chat-2-icon.png")));
 		gbc_btnChat.insets = new Insets(0, 0, 5, 0);
 		gbc_btnChat.gridx = 0;
-		gbc_btnChat.gridy = 2;
+		gbc_btnChat.gridy = 3;
 		pnlLeft.add(btnChat, gbc_btnChat);
 		
-		JButton btnConfigurations = new JButton("Config");
+		JButton btnConfigurations = new JButton("");
 		GridBagConstraints gbc_btnConfigurations = new GridBagConstraints();
+		btnConfigurations.setIcon(new ImageIcon(MainFrame.class.getResource("/icons/64/settings-3-icon.png")));
 		gbc_btnConfigurations.gridx = 0;
-		gbc_btnConfigurations.gridy = 3;
+		gbc_btnConfigurations.gridy = 4;
 		pnlLeft.add(btnConfigurations, gbc_btnConfigurations);
 	}
 
---
+++ JShare148252/src/main/java/br/univel/view/frames/HomeFrame.java
@@ -0,0 +1,14 @@
+package br.univel.view.frames;
+
+import javax.swing.JPanel;
+
+public class HomeFrame extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public HomeFrame() {
+
+	}
+
+}
